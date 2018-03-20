f6c7e6930e0b4dfee0bddfb4b24b1828c9456c44
mgrisole <mgrisole@gmail.com>
2018-03-20 22:31:04 +0100

    updates

--- src/typewriter.js
+++ src/typewriter.js
@@ -1,84 +1,3 @@
-// (function(root, factory) {
-//   if (typeof define === 'function' && define.amd) {
-//     // AMD. Register as an anonymous module.
-//     define([], factory);
-//   } else if (typeof module === 'object' && module.exports) {
-//     // Node. Does not work with strict CommonJS, but
-//     // only CommonJS-like environments that support module.exports,
-//     // like Node.
-//     module.exports = factory();
-//   } else {
-//     // Browser globals (root is window)
-//     root.typewriter = factory();
-//   }
-// }(this, () => {
-//
-//   function typeit($el, text, speed){
-//     var schedule = ( function f(){
-//       setTimeout( () => {
-//         if( text.length ){
-//           $el[0].childNodes[0].replaceWith( $el[0].childNodes[0].wholeText + text.shift() );
-//           schedule();
-//         } else {
-//           $el.find('.typeme-cursor').addClass('finished');
-//         }
-//       }, ( / /.test(text[0]) ) ? 0 : speed + Math.floor(Math.random() * Math.floor(40) ));
-//
-//       return f;
-//     })();
-//   }
-//
-//   function typewriter( params ){
-//     let t = params.speed;
-//     let $el = params.el;
-//     let cursor = params.cursor;
-//
-//     if( params.fixePosition ){
-//
-//       $el
-//         .css('visibility','hidden')
-//         .text( params.text.join('') + ( cursor || '' ) )
-//         .css({
-//           'width': $el.width(),
-//           'height': $el.height()
-//         })
-//         .text('')
-//         .css('visibility','visible')
-//       ;
-//     }
-//
-//     $el.text(' ');
-//     if( cursor ){
-//       let cursorStyle = `
-//         <style type="text/css" class="typeme cursor">
-//
-//           @keyframes blink {
-//             0% {
-//               opacity: 1 }
-//             50% {
-//               opacity: 0 }
-//             100% {
-//               opacity: 1 }
-//           }
-//
-//           .typeme-cursor.finished {
-//             opacity: 1;
-//             animation: blink .7s infinite;
-//           }
-//         </style>`;
-//       $('head').append(cursorStyle);
-//       $el.append('<span class="typeme-cursor">' + cursor + '</span>')
-//     }
-//
-//     typeit($el, params.text, params.speed);
-//   }
-//
-//   typewriter.version = '1.0.0';
-//
-// return typewriter;
-//
-// }));
-
 export default class Typewriter {
 
   constructor(params) {
@@ -87,22 +6,23 @@ export default class Typewriter {
     this.speed = params.speed / 2;
     this.humanize = params.humanize === undefined ? true : params.humanize;
     this.fixePosition = params.fixePosition;
-    this.text = params.text === undefined ? '' : params.text;
+    this.text = params.text;
     this.ignoreWhitespace = params.ignoreWhitespace === undefined ? false : params.ignoreWhitespace;
     this.synchroniseCursors = params.synchroniseCursors === undefined ? true : params.synchroniseCursors;
     this.writingSequences = this.setText();
+
+    console.log( this.writingSequences );
 
     this.typeit();
   }
 
   setText() {
-    return Array.prototype.map.call( document.querySelectorAll( this.target ), e => {
-      console.log(this);
+    return Array.from( document.querySelectorAll( '.typeMe'), e => {
       return {
         target: e,
-        text: [...e.dataset.typeit] || [...this.text]
-      };
-    });
+        text: Array.from( ( e.dataset.typeit || this.text ) || e.textContent )
+      }
+    })
   }
 
   setCursor( target ) {
