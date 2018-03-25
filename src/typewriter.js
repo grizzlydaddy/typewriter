73d8caf055cf771fc6dfd6dfcc3e79dcd1ea91d3
mgrisole <grisole.maxime@gmail.com>
2018-03-25 01:00:03 +0100

    Updates

--- src/typewriter.js
+++ src/typewriter.js
@@ -63,51 +63,37 @@ export default class Typewriter {
   }
 
   backspace (sequence) {
+    // console.log(sequence.text);
+    // sequence.textNode.nodeValue += sequence.text.shift();
   }
 
-  misstype (sequence) {
-    let trueChar = /\s/.test(sequence.text[0]) ? null : sequence.text.shift();
-    if ( trueChar ) {
+  misstype (sequence, callback) {
+    let trueChar = sequence.text[0];
+    if ( trueChar && !/\s/.test(trueChar) ) {
       let isUpperCase = trueChar === trueChar.toUpperCase();
       trueChar = trueChar.toLowerCase();
-      let wrongChar = trueChar;
-      if ( new RegExp( '[' + this.keyboard.join('') + ']' ).test(trueChar) ) {
-        let keyboardLine = this.keyboard.filter( e => { return new RegExp('['+ e +']').test(trueChar) });
+      if ( this.keyboard.join().indexOf(trueChar) >= 0 ) {
+        let keyboardLine = this.keyboard.filter( e => { return  e.indexOf(trueChar) >= 0 });
         if ( keyboardLine.length ) {
           keyboardLine = keyboardLine[0];
-          let keyboardLinePosition = this.keyboard.indexOf(keyboardLine);
-          // let siblingLine = this.keyboard[keyboardLinePosition + (keyboardLinePosition === 0 ? 1 : -1 )];
           let letterPosition = keyboardLine.indexOf(trueChar.toLowerCase());
-          console.log(keyboardLine);
-          console.log(letterPosition);
-          console.log(trueChar);
-          // console.log( siblingLine );
-          wrongChar = ((!letterPosition||letterPosition+1 === keyboardLine.length) ? keyboardLine[ letterPosition + (letterPosition ? -1:1)] : keyboardLine[letterPosition + (parseInt(Math.random()*100 % 2) ? 1 : -1)] );
-
-          // if ( letterPosition === 0 ) {
-          //   if ( keyboardLinePosition === 0) {
-          //     wrongChar = parseInt(Math.random()*100) % 2 && this.keyboard[keyboardLinePosition+1] ? this.keyboard[keyboardLinePosition+1][0] : keyboardLine[1];
-          //   }
-          // } else {
-          //   wrongChar = 'w';
-          // }
+          let wrongChar = ((!letterPosition||letterPosition+1 === keyboardLine.length) ? keyboardLine[ letterPosition + (letterPosition ? -1:1)] : keyboardLine[letterPosition + (parseInt(Math.random()*100 % 2) ? 1 : -1)] );
+          sequence.text.unshift( isUpperCase ? wrongChar.toUpperCase() : wrongChar);
         }
-      } else if ( !isNaN(parseInt(trueChar)) ) {
-        console.log(54);
       }
-      sequence.text.unshift(wrongChar);
     }
+
+    return true;
   }
 
   typeLetters (sequence, speed = this.speed) {
+    let misstyped = false;
     if ( this.humanize ) {
       speed = Math.abs(Math.random() * this.speed + this.speed/2);
       speed = Math.round(speed) % 2 && speed > this.speed / 0.25 ? this.speed / 2 : speed;
     }
-    let rand = Math.random() * 100;
-    // console.log( rand );
-    if ( this.mistype && this.mistypeRate > rand ) {
-      this.misstype(sequence);
+    if ( this.mistype && this.mistypeRate > Math.random() * 100 ) {
+      misstyped = this.misstype(sequence);
     }
     setTimeout( () => {
       if ( sequence.text.length ) {
