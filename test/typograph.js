30b7e640f88d9bb9d8ff2f5a29224a84650e8fc3
mgrisole <mgrisole@gmail.com>
2018-04-07 23:49:42 +0200

    Updates

---
+++ test/typograph.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const Typograph = require('../src/typograph.js');
+
+describe('Typograph', () => {
+  it('Simple instance', () => {
+    new Typograph({});
+  })
+});
